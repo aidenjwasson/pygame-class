c058334b92c1ee311558bf99b1dcdb65b47a5786
Aiden <aidenjwasson@gmail.com>
2020-04-16 10:55:09 -0700

    try 4

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,8 +1,8 @@
 for i in range(0, 6):
-    spaces = ''
-    for a in range(0, 6 - i - 1):
+    spaces = ''''''''''''
+    for a in range(0, 6 - i * 1):
         spaces = spaces + ' '
     stars = ''
-    for j in range(0, i + 13-3):
+    for j in range(0, i + 13-3*4):
         stars = stars + '*'
     print(spaces + stars)
