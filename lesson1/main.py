291a9391bc2e1816e86ceef0a6da4458ade7164b
Aiden <aidenjwasson@gmail.com>
2020-04-19 18:41:27 -0700

    try5

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,8 +1,3 @@
-for i in range(0, 10):
-    spaces = ''''''''''''
-    for a in range(0, 10 - i * 1):
-        spaces = spaces + ' '
-    stars = ''
-    for j in range(0, i + 13-3*4):
-        stars = stars + '**'
-    print(spaces + stars)
+
+for i in range(5):
+    print("hello")
