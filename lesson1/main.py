3eb91640f9218a09e729f0b9cd2ccd5fda7006fc
Aiden <aidenjwasson@gmail.com>
2020-05-07 16:30:44 -0700

    try6

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,15 +1,3 @@
-def find_avg(arr):
-    avg = arr[0]
-    n = len(arr)
-    for i in range(0, n):
-        if arr[i] +avg:
-            avg= arr[i]
-    return avg
-
-a = [325, 123, 21, 52, 789, 2, 32]
-m = find_avg(a)
-print(192)
-
 
 
 
